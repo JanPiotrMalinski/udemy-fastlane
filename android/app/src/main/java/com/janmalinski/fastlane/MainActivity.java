ba87be132f9442a6e56fb77dda02ce723b92e22e
Jan Malinski <janmalinski@MacBook-Pro-Jan.local>
2023-09-15 13:40:23 +0200

    rectify bundle identifier name for ios and android

--- android/app/src/main/java/com/janmalinski/fastlane/MainActivity.java
+++ android/app/src/main/java/com/janmalinski/fastlane/MainActivity.java
@@ -1,4 +1,4 @@
-package com.janmalinski.udemy.fastlane;
+package com.janmalinski.fastlane;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/janmalinski/fastlane/MainApplication.java
+++ android/app/src/main/java/com/janmalinski/fastlane/MainApplication.java
@@ -1,4 +1,4 @@
-package com.janmalinski.udemy.fastlane;
+package com.janmalinski.fastlane;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
